a6b5452a66d26212cd0d1150f7c184dec4ce5ae6
BashkoAnastasya <nastinm@mail.ru>
2018-03-29 18:10:53 +0300

    добавление задания 2

---
+++ calc.java
@@ -0,0 +1,61 @@
+﻿import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double num1 = getDouble();
+        double num2 = getDouble();
+        char operation = getOperation();
+        double result = calc(num1,num2,operation);
+        System.out.println("Результат: "+result);
+    }
+
+    public static double getDouble(){
+        System.out.println("Введите число:");
+        double num;
+        if(scanner.hasNextDouble()){
+            num = scanner.nextDouble();
+        } else {
+            System.out.println("Ошибка. Попробуйте еще раз.");
+            scanner.next();//рекурсия
+            num = getDouble();
+        }
+        return num;
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Ошибка. Попробуйте еще раз.");
+            scanner.next();//рекурсия
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static double calc(double num1, double num2, char operation){
+        double result;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Ошибка. Повторите ввод.");
+                result = calc(num1, num2, getOperation());//рекурсия
+        }
+        return result;
+    }
+}
